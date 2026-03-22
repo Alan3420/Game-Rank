dc7682092fbe3d478ddad2e35aa8b34e3d15fd5f
Alan <alan.n.m143@gmail.com>
2026-03-22 19:36:21 +0100

    primeros diseños de la pagina y comprobaciones de coñexiones con el back, apartado home y login primeros diseños

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,19 @@
 <template>
+   <header>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="/"><img src="" alt="logo_game_rank"></a></li>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="#">Reseñas</a></li>
+                <li><a href="#">Juegos</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
   <RouterView />
+
+  <footer>
+        <p>&copy; 2026 Game Rank. Todos los derechos reservados.</p>
+    </footer>
 </template>
--- frontend/src/view/login_user.vue
+++ frontend/src/view/login_user.vue
@@ -1,13 +1,30 @@
 <template>
-    <form @submit.prevent="handleLogin">
-        <label for="email">Correo Electrónico:</label>
-        <input id="email" v-model="email" type="email" required>
+    <head>
+        <title>Login - Game Rank</title>
+    </head>
 
-        <label for="password">Contraseña:</label>
-        <input id="password" v-model="password" type="password" required>
+    <div class="container-login">
+        <div class="welcome-section">
+            <h1>Bienvenido de vuelta</h1>
+            <p>Por favor, inicia sesión para continuar.</p>
+            <a href="https://github.com/Alan3420/Proyecto-Game-Rank.git" target="_blank" class="github-link">
+                <i class="fab fa-github"></i>Repo Github
+            </a>
+        </div>
 
-        <button type="submit">Iniciar Sesión</button>
-    </form>
+        <fieldset>
+            <legend>Iniciar Sesión</legend>
+            <form @submit.prevent="handleLogin">
+            <label for="email">Correo Electrónico:</label>
+            <input id="email" v-model="email" type="email" required>
+
+            <label for="password">Contraseña:</label>
+            <input id="password" v-model="password" type="password" required>
+
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+        </fieldset>
+    </div>
 </template>
 
 <script>
@@ -32,3 +49,95 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.container-login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  min-height: 80vh;
+  padding: 2rem;
+}
+.welcome-section ,fieldset{
+  flex: 1;
+  max-width: 400px;
+}
+.welcome-section{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 1rem;
+}
+
+fieldset{
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 2rem;
+}
+
+form{
+    display: flex;
+    flex-direction: column;
+
+}
+
+form label {
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+form input {
+  margin-bottom: 15px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+form button {
+  background-color: #6366f1;
+  color: #ffffff;
+  padding: 10px;
+  font-size: 1rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.25s ease, transform 0.2s ease;
+}
+form button:hover {
+  background-color: #4f46e5;
+  transform: translateY(-2px);
+}
+
+a i {
+  font-size: 1.5rem; 
+  color: #111827;     
+  transition: color 0.2s ease;
+}
+
+
+.github-link {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+  text-decoration: none;
+
+  color: #111827;
+  padding: 1rem 5rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+.github-link:hover, .github-link:hover i {
+  background-color: #6366f1;
+  color: white;
+  
+}
+
+@media (max-width: 768px) {
+    .container-login {
+        flex-direction: column;
+        padding: 1rem;
+    }
+}
+</style>
