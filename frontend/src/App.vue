8a2f0fb4359252531af21d5476d1f81b66888a46
Alan Novas Mateo <alan.novas@citic-research.org>
2026-03-23 13:23:47 +0100

    jwt implementado actualizaciones en router para proteger rutas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
                 <li><a href="/"><img src="" alt="logo_game_rank"></a></li>
                 <li><a href="/">Inicio</a></li>
                 <li><a href="#">Reseñas</a></li>
-                <li><a href="#">Juegos</a></li>
+                <li><a href="/content">Juegos</a></li>
                 <li><a href="#">Contacto</a></li>
             </ul>
         </nav>
