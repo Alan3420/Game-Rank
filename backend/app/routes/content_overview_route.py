7eee9cfb0b87e244510304dccc8ee75748dd22d9
Alan3420 <alan.n.m143@gmail.com>
2026-03-27 22:57:34 +0100

    actualizacion de envio de datos de pruebas al front

--- backend/app/routes/content_overview_route.py
+++ backend/app/routes/content_overview_route.py
@@ -1,13 +1,12 @@
 from flask import Blueprint, jsonify
-from flask_jwt_extended import get_jwt_identity, jwt_required
+
 
 
 content_overview_bp = Blueprint('content_overview_route', __name__)
 
 @content_overview_bp.route('/overview')  
-
 def get_content():
-    token = get_jwt_identity()
+
     temporal_message = "Hola, bienvenido a Game Rank, tu destino para descubrir y compartir tus experiencias de juego. Explora reseñas, calificaciones y recomendaciones personalizadas para encontrar tu próxima aventura gamer. ¡Únete a nuestra comunidad y comparte tu pasión por los videojuegos!"
 
-    return jsonify({"message":temporal_message, "user_id":token}), 200
+    return jsonify({"message":temporal_message})
--- frontend/src/view/content_overview.vue
+++ frontend/src/view/content_overview.vue
@@ -14,13 +14,18 @@ import { getContentOverview } from "../services/content_service";
 export default {
     data() {
         return {
-            contentOverview: null
+            contentOverview: null,
+            user_id: null
         }
     },
     async mounted() {
         try {
-            this.contentOverview = await getContentOverview();
-            document.getElementById("content").textContent= this.contentOverview
+            const response = await getContentOverview(this.contentOverview, this.user_id);
+            document.getElementById("content").textContent= JSON.stringify(response.message)
+            console.log(JSON.stringify(response.message));
+            const user = JSON.parse(localStorage.getItem("user"))
+            console.log(user.id_user);
+            
         } catch (error) {
             console.error('Error fetching content overview:', error);
         }
@@ -38,7 +43,7 @@ export default {
 
 <style scoped>
 .content-overview{
-    min-height: 100vh;
+    min-height: 80vh;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
@@ -50,5 +55,19 @@ export default {
     .content-overview{
         flex: 1;
     }
+}
+button{
+    padding: 1rem 3rem;
+    border-radius: 10px;
+    border: 1px solid white;
+    color: white;
+    background-color: black;
+    cursor: pointer;
+
+}
+button:hover{
+    box-shadow: inset 0 0 3px 1px black;
+    background-color: grey;
+    
 }
 </style>
