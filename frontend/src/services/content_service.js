ccaacc31327b94f0385ec24dd5a2a69fbe8d2e3f
Alan3420 <alan.n.m143@gmail.com>
2026-03-27 22:57:34 +0100

    actualizacion de envio de datos de pruebas al front

--- frontend/src/services/content_service.js
+++ frontend/src/services/content_service.js
@@ -1,11 +1,15 @@
 import api from './api';
 
-export const getContentOverview = async () => {
+export async function getContentOverview (contentOverview, user_id) {
     try {
-        const response = await api.get('/content/overview');
+        const response = await api.get('/content/overview',{
+            contentOverview: contentOverview,
+            user_id: user_id
+        });
+
         return response.data;
     } catch (error) {
-        console.error('Error fetching content overview:', error);
+        console.error('Error al cargar el resumen del contenido:', error);
         throw error;
     }
 };
