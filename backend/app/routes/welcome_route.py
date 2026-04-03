39fe56e7560176c91a756b55c4f3422c8fdc3e47
Alan3420 <alan.n.m143@gmail.com>
2026-04-03 15:54:36 +0200

    Manejo de errores en back y front actualizacion de los formularios y comprobacion de version de axios por reciente hacking al mantenedor

--- backend/app/routes/welcome_route.py
+++ backend/app/routes/welcome_route.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import IntegrityError
 from app.services import user_service
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
@@ -19,26 +20,28 @@ def login():
                         "user": user.to_dict(),
                         "token": token_user}), 200
     else:
-        return jsonify({"message": "Invalid email or password"}), 401
+        return jsonify({"message": "Correo electronico o contraseña incorrectos"}), 401
 
 @welcome_bp.route("/register", methods=["POST"])
 def register():
-    register_data = request.get_json()
+    try:
+        register_data = request.get_json()
 
-    name_user = register_data.get("name")
-    last_name_user = register_data.get("last_name")
-    email_user = register_data.get("email")
-    password_user = register_data.get("password")
-    user = user_service.user_registration(name=name_user, last_name=last_name_user, email=email_user, passwd=password_user)
+        name_user = register_data.get("name")
+        last_name_user = register_data.get("last_name")
+        email_user = register_data.get("email")
+        password_user = register_data.get("password")
+        user = user_service.user_registration(name=name_user, last_name=last_name_user, email=email_user, passwd=password_user)
 
-    if user:
+        if user:
 
-        return jsonify({"message": "Registration successful", 
-                        "user": user.to_dict()}), 201
-    else:
-        return jsonify({"message": "Email already exists"}), 400
-
+            return jsonify({"message": "Usuario registrado exitosamente", 
+                            "user": user.to_dict()}), 201
+    except IntegrityError:
+        return jsonify({"message": "El correo electrónico ya está registrado"}), 400
+    except Exception as e:
+        return jsonify({"message": "Error al registrar el usuario", "error": str(e)}), 500
 
 @welcome_bp.route("/logout")
 def logout():
-    token = get_csrf_token()
+    return jsonify({"message": "Logout successful"}), 200
--- frontend/src/view/login_user.vue
+++ frontend/src/view/login_user.vue
@@ -1,56 +1,88 @@
 <template>
 
-    <body>
-        <fieldset>
-            <legend>Iniciar Sesion</legend>
+  <body>
+    <fieldset>
+      <legend>Iniciar Sesion</legend>
 
-            <form @submit.prevent="handleLogin">
-                <label for="email">Correo Electrónico:</label>
-                <input type="email" id="email" v-model="email" placeholder="email">
+      <form @submit.prevent="handleLogin">
+        <label for="email">Correo Electrónico:</label>
+        <input type="email" id="email" v-model="email" placeholder="email">
 
-                <label for="passwd">Contraseña:</label>
-                <input type="password" id="passwd" v-model="password" placeholder="password">
+        <label for="passwd">Contraseña:</label>
+        <input type="password" id="passwd" v-model="password" placeholder="password">
 
-                <button type="submit">Login</button>
-                <a href="/register">¿no tienes cuenta? Registrate</a>
-            </form>
-        </fieldset>
-    </body>
+        <button type="submit" :disabled="loading">
+          <span v-if="!loading">Login</span>
+          <span v-else class="dots-loader">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </button>
+        <a href="/register">¿no tienes cuenta? Registrate</a>
+        <p class="informacion"></p>
+      </form>
+    </fieldset>
+  </body>
 </template>
 
 <script>
 import { login } from "../services/user_service";
 
 export default {
-    data() {
-        return {
-            email: "",
-            password: ""
+  data() {
+    return {
+      email: "",
+      password: "",
+      loading: false
 
-        }
-    },
-    methods: {
-        async handleLogin() {
-            try {
-                const response = await login(this.email, this.password)
-                localStorage.setItem("token", response.token)
-                localStorage.setItem("user", JSON.stringify(response.user))
-                this.$router.push('/content/overview');
-            } catch (error) {
-                console.log("Error:", error)
-            }
-        }
     }
+  },
+  methods: {
+    async handleLogin() {
+      try {
+        this.loading = true
+        const response = await login(this.email, this.password)
+        
+
+        await new Promise(resolve => setTimeout(resolve, 2000))
+
+        console.log("Inicio de Sesion correcto");
+
+        localStorage.setItem("token", response.token)
+        localStorage.setItem("user", JSON.stringify(response.user))
+        this.$router.push('/content/overview');
+
+      } catch (error) {
+        if(error.response && error.response.status === 401){
+          
+          await new Promise(resolve => setTimeout(resolve, 2000))
+          let classInfo = document.querySelector(".informacion")
+          classInfo.id = "info";
+          
+          let info = document.getElementById("info")
+          info.textContent = error.response.data.message
+        }
+        else{
+          console.log("Error:", error.message);
+        }
+      }
+      finally {
+        this.loading = false
+      }
+
+    }
+  }
 }
 </script>
 
 
 <style scoped>
 body {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
 }
 
