4da7e0b11e17d80ba9882907ae197b359f0b4c4c
Alan Novas Mateo <alan.novas@citic-research.org>
2026-04-07 12:31:28 +0200

    cambios en la pagina de bienvenida + logo (front)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
    <header>
         <nav>
             <ul class="nav-links">
-                <li><a href="/"><img id="logo" src="/src/assets/logo_GR.png" alt="logo_game_rank"></a></li>
+                <li><a href="/"><img id="logo" src="/src/assets/game_rank_logo.png" alt="logo_game_rank"></a></li>
                 <div class="user_container">
                     <li><a href="/login">Iniciar Sesión</a></li>
                     <li><a href="/register">Registrarse</a></li>
@@ -11,7 +11,9 @@
         </nav>
     </header>
 
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 
   <footer>
         <p>&copy; 2026 Game Rank. Todos los derechos reservados.</p>
