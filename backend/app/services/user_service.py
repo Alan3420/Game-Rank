b95df67a64785bc316c2a953adc5a1de10dc8d66
Alan3420 <alan.n.m143@gmail.com>
2026-04-10 14:56:33 +0200

    Mejora en la función de registro de usuario para manejar errores y verificar existencia previa

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -12,12 +12,26 @@ def user_authentication(email, passwd) -> User |None:
     return None
 
 
-def user_registration(name, last_name, email, passwd) -> User:
+def user_registration(name, last_name, email, passwd) -> User | str:
 
-    if user_authentication(email, passwd) == None:
+    try:
+        user_exist = user_repo.get_user_by_email(email)
+
+        if user_exist is not None:
+            return "Existe un usuario registrado con ese email"
+       
         new_user = user_repo.create_user(username=name, last_name=last_name, email=email, password=passwd)
 
         return new_user
+        
+        
+    
+    except Exception as e:
+        print("Error en el registro del usuario:", str(e))
+        raise Exception("Error en el registro del usuario")
+    
+    
+    
 
 
 
