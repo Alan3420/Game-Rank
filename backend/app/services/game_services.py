1a9d22826586ada7fb7ed91ef793a823a5233bbf
Alan3420 <alan.n.m143@gmail.com>
2026-04-12 13:37:31 +0200

    Reestructura de las funciones del repositorio de videojuegos para mejorar la integración de la API y la gestión de errores

--- backend/app/services/game_services.py
+++ backend/app/services/game_services.py
@@ -1,33 +1,43 @@
-from app.repositories.vGame_repo import get_game_by_id, get_all_video_games, get_game_by_name, create_video_game
+from app.repositories.vGame_repo import create_video_game, get_game_by_id_api, get_game_by_id_bd, get_game_by_name
 from app.services.adapter import game_format
 
 def get_video_game_details(game_id) -> dict:
-    game_details = get_game_by_id(game_id=game_id)
-    all_games = get_all_video_games()
+    try:
+        game_details = get_game_by_id_api(game_id=game_id)
 
-    game_exists = False
+        game_exists = get_game_by_id_bd(game_id=game_id)
 
-    for game in all_games:
-        if game.id_game_api ==game_details["id"]:
-            game_exists = True
-            break
+        if not game_exists:
+            listaPlataformas = ""
+            listaDevs_Company = ""
+            for plataforma in game_details["platforms"]:
+                listaPlataformas += plataforma["platform"]["name"]
+                if plataforma != game_details["platforms"][-1]:
+                    listaPlataformas += ", "
+            
+            for dev_company in game_details["developers"]:
+                listaDevs_Company += dev_company["name"]
+                if dev_company != game_details["developers"][-1]:
+                    listaDevs_Company += ", "
+            
+            create_video_game(id_game_api=game_details["id"], 
+                            name=game_details["name"], 
+                            date_release=game_details["released"], 
+                            platforms=listaPlataformas, 
+                            development_company=listaDevs_Company,
+                            id_comment=None)
 
-    if not game_exists:
-        create_video_game(id_game_api=int(game_details["id"]), 
-                              name=str(game_details["name"]), 
-                              date_release=game_details["released"], 
-                              platforms=str(game_details["platforms"][0]["platform"]["name"]), 
-                              development_company=str(game_details["developers"][0]["name"]), 
-                              id_comment=None)
 
-    return game_format(game_details)
+        return game_format(game_details)
+    except Exception as e:
+        raise Exception(f"Error al obtener los detalles del juego: {str(e)}")
 
 def get_video_game_by_name_details(game_name) -> dict | list[dict]:
     name_game_details = get_game_by_name(game_name=game_name)
 
     id_game_details = name_game_details["results"][0]["id"]
 
-    game_details = get_game_by_id(game_id=id_game_details)
+    game_details = get_game_by_id_api(game_id=id_game_details)
 
     if not name_game_details["results"]:
         raise Exception("No se encontró ningún juego con ese nombre")   
