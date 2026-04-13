2c0e7ac65c906fd84a4b7e29c25c3d62786e5422
Alan3420 <alan.n.m143@gmail.com>
2026-04-13 10:06:28 +0200

    Funciones de actualización y eliminación de usuario, y se añade verificación de existencia de usuario y correo electrónico.

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -29,9 +29,60 @@ def user_registration(name, last_name, email, passwd) -> User | str:
     except Exception as e:
         print("Error en el registro del usuario:", str(e))
         raise Exception("Error en el registro del usuario")
+
+def user_update(user_id, name, last_name, email, passwd) -> User | str:
+
+    try:
+        user = user_repo.get_user_by_id(user_id)
+
+        if not user:
+            return "Usuario no encontrado"
+
+        if email and email != user.email:
+            email_exists = user_repo.get_user_by_email(email)
+            if email_exists:
+                return "El correo electrónico ya está en uso por otro usuario"
+
+        updated_user = user_repo.update_user(user_id=user_id, username=name, last_name=last_name, email=email, password=passwd)
+
+        return updated_user
     
+    except Exception as e:
+        print("Error al actualizar el usuario:", str(e))
+        raise Exception("Error al actualizar el usuario")
     
+def user_delete(user_id) -> bool | str:
+
+    try:
+        user = user_repo.get_user_by_id(user_id)
+
+        if not user:
+            return "Usuario no encontrado"
+
+        resultado_bool = user_repo.delete_user(user_id)
+
+        return resultado_bool
     
+    except Exception as e:
+        print("Error al eliminar el usuario:", str(e))
+        raise Exception("Error al eliminar el usuario")
+    
+def change_role(user_id, new_role) -> User | str:
+
+    try:
+        user = user_repo.get_user_by_id(user_id)
+
+        if not user:
+            return "Usuario no encontrado"
+
+        updated_user = user_repo.update_user_role(user_id=user_id, new_role=new_role)
+
+        return updated_user
+    
+    except Exception as e:
+        print("Error al cambiar el rol del usuario:", str(e))
+        raise Exception("Error al cambiar el rol del usuario")
+
 
 
 
