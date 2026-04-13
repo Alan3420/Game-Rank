280153dfc530a171e87314097eb96a5db73b7fca
Alan3420 <alan.n.m143@gmail.com>
2026-04-13 15:01:44 +0200

    Se añade PrimeVue y primeicons, y mejora el diseño del buscador en la vista de contenido

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router/index.js"
+import 'primeicons/primeicons.css'
 import "./style.css"
 
+import PrimeVue from "primevue/config"
+
 const app = createApp(App)
+
 app.use(router)
+
+app.use(PrimeVue)
+
 app.mount("#app")
