5442c1b6f3da9f6b9e30acce223c35b72056fcdb
Alan3420 <alan.n.m143@gmail.com>
2026-04-14 11:41:24 +0200

    Añade soporte para conexión segura a la base de datos (Aiven) y ajusta las relaciones de comentarios y videojuegos para eliminar claves foráneas obsoletas.

--- backend/app/models/Comment.py
+++ backend/app/models/Comment.py
@@ -7,12 +7,11 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id_comment = Column(Integer, primary_key=True, unique=True,autoincrement=True)
-    id_user = Column(Integer, ForeignKey("users.id_user"),primary_key=True)
+    id_user = Column(Integer, ForeignKey("users.id_user", ondelete="CASCADE"),primary_key=True)
+    id_videogame = Column(Integer,ForeignKey("videoGame.id_game_api", ondelete="CASCADE"), primary_key=True)
     description = Column(String(255), nullable=False)
     date_of_comment = Column(Date, nullable=False, default=date.today)
 
     users_rl = relationship('User',back_populates="comments_rl")
-    video_game_rl = relationship("Video_game", foreign_keys="[Video_game.id_comment]", back_populates="comments_rl")
-
 
 
